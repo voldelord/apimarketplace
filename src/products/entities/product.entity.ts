54e2622e268139708a964c096c947bd1cc3b3855
ProjectsReactJs <reactjsoriginprojects@gmail.com>
2023-08-29 17:36:51 -0400

    producto y usuarios

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,40 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: string;
+
+  @Column()
+  category: string;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  quantityinstock: number;
+
+  @Column()
+  weight: number;
+
+  @Column()
+  dimensions: string;
+
+  @Column()
+  origincountry: string;
+
+  @Column()
+  status: number;
+}
